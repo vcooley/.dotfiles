7dd787f0421b501142bb7cb823000bd6bff8b251
Vince Cooley <vince.r.cooley@gmail.com>
2024-05-15 14:58:32 -0400

    remove autopairs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -48,23 +48,6 @@ return {
   },
 
   {
-    'windwp/nvim-autopairs',
-    enabled = false,
-    event = { 'InsertEnter' },
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function(_, opts)
-      local autopairs = require 'nvim-autopairs'
-      autopairs.setup(opts)
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-    opts = {
-      check_ts = true,
-      ts_config = { lua = 'string', javascript = 'template_string' },
-    },
-  },
-  {
     dir = '~/Projects/yab.nvim',
     init = function()
       require('yab').setup()
