dcbbd91b6670daa787dd4c5bcb9b1873241dec29
Vince Cooley <vince.r.cooley@gmail.com>
2024-12-05 20:20:34 -0700

    add dock settings to macos script

--- .config/macos.sh
+++ .config/macos.sh
@@ -1,3 +1,10 @@
 # Some users mention the key repeat setting may get clobbered during updates or when modifying other Keyboard settings
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# show/hide dock instantly
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+killall Dock
+
