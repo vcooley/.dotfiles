c72614104212631ec528e4b95db67acad24c680c
Vince Cooley <vince.r.cooley@gmail.com>
2024-12-06 08:18:00 -0700

    merge macos.sh

--- .config/macos.sh
+++ .config/macos.sh
@@ -1,3 +1,5 @@
+#! /usr/local/bin/bash
+
 # Some users mention the key repeat setting may get clobbered during updates or when modifying other Keyboard settings
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
@@ -12,5 +14,5 @@ killall Dock
 defaults write com.apple.dock expose-group-apps -bool true && killall Dock
 defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
 
-
-
+# Disable animations
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
